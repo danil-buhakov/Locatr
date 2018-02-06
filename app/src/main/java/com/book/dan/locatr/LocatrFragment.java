b530336125390158ebd83a22281280af9370a673
Danil Buhakov <tradinal@gmai.com>
2018-02-06 21:38:32 +0200

    Added runtime request premissions and Location listener

--- app/src/main/java/com/book/dan/locatr/LocatrFragment.java
+++ app/src/main/java/com/book/dan/locatr/LocatrFragment.java
@@ -1,8 +1,15 @@
 package com.book.dan.locatr;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -10,11 +17,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class LocatrFragment extends Fragment {
+    private static final String TAG = "LocatrFragment";
+    private static final String[] LOCATION_PERMISSIONS = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+    private static final int REQUEST_LOCATE_PERMISSIONS=0;
+
     private ImageView mImageView;
     private GoogleApiClient mClient;
 
@@ -53,6 +70,31 @@ public class LocatrFragment extends Fragment {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case REQUEST_LOCATE_PERMISSIONS:
+                if(hasLocatePermissions())
+                    findImage();
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.action_locate:
+                if(hasLocatePermissions())
+                    findImage();
+                else
+                    requestPermissions(LOCATION_PERMISSIONS,REQUEST_LOCATE_PERMISSIONS);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     public void onStart() {
         super.onStart();
 
@@ -66,6 +108,26 @@ public class LocatrFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_locatr,container, false);
         mImageView = v.findViewById(R.id.image);
         return v;
+    }
+
+    @SuppressLint("MissingPermission")
+    private void findImage(){
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setNumUpdates(1);
+        locationRequest.setInterval(0);
+        LocationServices.FusedLocationApi
+                .requestLocationUpdates(mClient,locationRequest,new LocationListener(){
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        Log.i(TAG,"Got a fix: "+location);
+                    }
+                });
+    }
+
+    private boolean hasLocatePermissions(){
+        int result = ContextCompat.checkSelfPermission(getActivity(),LOCATION_PERMISSIONS[0]);
+        return result== PackageManager.PERMISSION_GRANTED;
     }
 
     @Override
